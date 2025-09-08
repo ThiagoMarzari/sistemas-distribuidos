b7f8ee8526b101ff05f90fbcf76e241e4db179f0
ThiagoMarzari <87370358+ThiagoMarzari@users.noreply.github.com>
2025-09-08 09:39:39 -0300

    alt

--- Atividade- Thiago Marzari - Gabriel Pinheiro/src/PopulaLista.java
+++ Atividade- Thiago Marzari - Gabriel Pinheiro/src/PopulaLista.java
@@ -2,10 +2,10 @@ import java.util.List;
 import java.util.Random;
 
 class PopulaLista extends Thread {
-    private List<Integer> lista;
+    private List<List> lista;
     private int tamanho;
 
-    public PopulaLista(List<Integer> lista, int tamanho) {
+    public PopulaLista(List<List> lista, int tamanho) {
         this.lista = lista;
         this.tamanho = tamanho;
     }
@@ -15,7 +15,7 @@ class PopulaLista extends Thread {
         System.out.println("Thread " + this.getName() + " iniciada...");
         Random rand = new Random();
         for (int i = 0; i < tamanho; i++) {
-            lista.add(rand.nextInt(100000 - 1000) + 1000); // números entre 1000 e 100000
+            lista.add(rand.nextInt(100000 - 1000) + 1000); // números entre 1.000 e 100.000
         }
         System.out.println("Thread " + this.getName() + " finalizada!");
     }
